2e42e4f133e800813d26922262b09d5c2e727afa
Diogo Martins <diogo.martins1818@gmail.com>
2025-08-01 14:51:52 +0100

    Update hltv_demo_downloader.py
    - Fixed url validation
    - Revised print statements for cleaner output
    - Translated some comments to English

--- hltv_demo_downloader.py
+++ hltv_demo_downloader.py
@@ -10,9 +10,15 @@ from pathlib import Path
 
 def main():
     print("*** HLTV DEMO DOWNLOADER ***")
-    stat_page_link = input("Enter the stats page link: ")
-    folder_input = input("Enter the download folder path: ").strip()
 
+    while True:
+        stat_page_link = input("Enter the stats page link: ")
+        if is_valid_url(stat_page_link):
+            break
+        else:
+            print("[ERROR] Incorrect link format.")
+
+    folder_input = input("Enter the download folder path: ").strip()
     if not folder_input:
         default_path = Path.cwd() / "downloads"
         print(f"[!!!] No folder provided. Using default: {default_path}")
@@ -22,13 +28,9 @@ def main():
 
     folder_path.mkdir(parents=True, exist_ok=True)
     folder_path=str(folder_path)
+    
+    match_links = get_matches(stat_page_link, folder_path)
     failed_links = []
-
-    if is_valid_url(stat_page_link):
-        match_links = get_matches(stat_page_link, folder_path)
-    else:
-        print("[ERRO]Incorrect link format.")
-        quit()
 
     total_links = len(match_links)
     for index, link in enumerate(match_links, start=1): 
@@ -36,15 +38,17 @@ def main():
         try:
             get_demo(link, progress, folder_path)
         except DemoDownloadError as e:
-            print(f"[ERRO] {str(e)}")
+            print(f"[ERROR] {str(e)}")
+            failed_links.append(link)
             continue
         except Exception as e:
-            print(f"[CRITICAL ERROR] An unexpected error has occurred: {str(e)}")
+            print(f"[ERROR] An unexpected error has occurred: {str(e)}")
+            failed_links.append(link)
             continue
-        print(f"*** Sucess to download demo {index}: {link}")
+        print(f"*** Download completed ***")
 
     success_count = total_links - len(failed_links)
-    print(f"*** Finished: {success_count} of {total_links} demos downloaded successfully ***")
+    print(f"\n*** Finished: {success_count} of {total_links} demos downloaded successfully ***")
     if success_count != total_links:
         print(f"URL of failed links:")
         for i, link in enumerate(failed_links, start=1): print(f"{i} : URL: {link}")
@@ -62,13 +66,11 @@ def get_matches(stat_page_link, folder_path):
     match_links = []
     is_last_page = False
     while is_last_page == False:
-
         # Find matches with the dark background in this page (html match rectangle)
         elements2 = driver.find_elements(By.CSS_SELECTOR, ".group-2.first")
         
         # Find matches with a grey background in this page
         elements1 = driver.find_elements(By.CSS_SELECTOR, ".group-1.first")
-
 
         for i in range(min(len(elements2), len(elements1))):
             link2 = elements2[i].find_element(By.TAG_NAME, "a").get_attribute("href")
@@ -83,7 +85,6 @@ def get_matches(stat_page_link, folder_path):
 
         # Go to the next page if there are more matches left
         try: 
-            
             next_page = driver.find_element(By.CSS_SELECTOR, ".pagination-next")
             if next_page.get_attribute("href"):
                 next_page.click()
@@ -94,9 +95,8 @@ def get_matches(stat_page_link, folder_path):
     
     for link in match_links:
         print(link)
-    print(f"***Found {len(match_links)} demos.")
+    print(f"Found {len(match_links)} demos.")
 
-    
     driver.quit()
     return match_links
 
@@ -108,13 +108,10 @@ def get_demo(match_page: str, progress_number: int, folder_path: str):
     driver = firefox_driver(folder_path)
     try:
         driver.get(match_page)
-        
         _handle_cookies(driver)
-        
         _navigate_to_match_page(driver)
-        
-        download_btn = _get_download_button(driver)
-        _execute_download(progress_number, match_page, download_btn, folder_path)
+        download_button = _get_download_button(driver)
+        _execute_download(progress_number, match_page, download_button, folder_path)
     finally:
         driver.quit()
 
@@ -140,10 +137,11 @@ def _get_download_button(driver):
         raise DemoDownloadError("Download button not found - demo unavailable")
 
 def _execute_download(progress_number, match_page, download_btn, folder_path):
+    rename_old_part(folder_path)
+
     print(f"\n*** {progress_number} Demo download ***")
     print(f"Link: {match_page}")
 
-    rename_old_part(folder_path)
     download_btn.click()
     time.sleep(5)
 
@@ -155,24 +153,9 @@ def _execute_download(progress_number, match_page, download_btn, folder_path):
     if is_downloading(part_file):
         raise DemoDownloadError("Download stuck without progress")
 
-def firefox_driver(folder_path):
-    options = webdriver.FirefoxOptions()
-    #SETTINGS TO AVOID CONFIRMATION POPUPS  
-    options.set_preference("browser.download.manager.showWhenStarting", False)
-    options.set_preference("browser.download.manager.quitBehavior", 2)  # 0=Padrão, 1=Cancelar, 2=Continuar
-    options.set_preference("browser.download.manager.showAlertOnComplete", False)
-    options.set_preference("browser.download.manager.focusWhenStarting", False)    
-    
-    options.add_argument("-headless")
-    options.set_preference("browser.download.folderList", 2) 
-    options.set_preference("browser.download.dir", folder_path)
-    options.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/x-rar-compressed") 
-    driver = webdriver.Firefox(options=options) 
-    return driver
 
 #Waits for any visible .part, returns the most recent.
 def wait_for_new_part(folder, timeout_appear=30):
-    print("Starting to download...\n")
     start = time.time()
     latest = None
     latest_time = 0
@@ -180,32 +163,33 @@ def wait_for_new_part(folder, timeout_appear=30):
     while time.time() - start < timeout_appear:
         parts = list(Path(folder).glob("*.part"))
         if parts:
-            # Pega o mais recente baseado na última modificação
             newest = max(parts, key=lambda p: p.stat().st_mtime)
             mod_time = newest.stat().st_mtime
 
             if mod_time > latest_time:
                 latest = newest
                 latest_time = mod_time
-                break  # part realmente novo
+                break
 
         time.sleep(1)
 
     if latest:
         return latest
-    raise TimeoutError("Nenhum .part detectado.")
+    raise TimeoutError("Theres no .part in the folder")
 
-#in case there are .part files in the folder
+
+# In case there are already .part files in the download folder 
+# this will rename the files to avoid conflicts
 def rename_old_part(folder):
     for p in Path(folder).glob("*.part"):
         target = p.with_suffix(".part.old")
-        # Garante nome único caso já exista
-        idx = 1
+        index= 1
         while target.exists():
-            target = p.with_suffix(f".part.old{idx}")
-            idx += 1
-        print(f"Renomeando {p.name} → {target.name}")
+            target = p.with_suffix(f".part.old{index}")
+            index += 1
+        print(f"Renaming {p.name} → {target.name}")
         p.rename(target)
+
 
 def is_downloading(part_path, timeout_stall=300, check=5):
     last_size = -1
@@ -216,16 +200,33 @@ def is_downloading(part_path, timeout_stall=300, check=5):
         if size != last_size:
             last_size = size
             last_change = time.time()
+            print("Downloading...")
         elif time.time() - last_change > timeout_stall:
-            return True  # travou
+            return True
 
         time.sleep(check)
-    return False  # terminou
+    return False
 
 
 def is_valid_url(url):
     parsed = urlparse(url)
-    return all([parsed.scheme, parsed.netloc]) and "stats" in url
+    return all([parsed.scheme, parsed.netloc]) and "https://www.hltv.org/stats/" in url
+
+
+def firefox_driver(folder_path):
+    options = webdriver.FirefoxOptions()
+    #SETTINGS TO AVOID CONFIRMATION POPUPS  
+    options.set_preference("browser.download.manager.showWhenStarting", False)
+    options.set_preference("browser.download.manager.quitBehavior", 2)  # 0=default, 1=cancel, 2=continue
+    options.set_preference("browser.download.manager.showAlertOnComplete", False)
+    options.set_preference("browser.download.manager.focusWhenStarting", False)    
+    
+    options.add_argument("-headless")
+    options.set_preference("browser.download.folderList", 2) 
+    options.set_preference("browser.download.dir", folder_path)
+    options.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/x-rar-compressed") 
+    driver = webdriver.Firefox(options=options) 
+    return driver
 
 
 if __name__  == "__main__":
